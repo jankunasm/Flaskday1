a59ce754cf542eb93c754083aba7016b746def46
jankunasm <jankunasmatas11@gmail.com>
2021-11-10 23:55:16 -0600

    Homework day 3 commit.

--- car_inventory/models.py
+++ car_inventory/models.py
@@ -3,15 +3,21 @@ from flask_migrate import Migrate
 import uuid
 from datetime import datetime
 import sqlalchemy
-
 from werkzeug.security import generate_password_hash, check_password_hash
 
 import secrets
+from flask_login import LoginManager, UserMixin
+from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+ma = Marshmallow()
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.String, primary_key = True)
     first_name = db.Column(db.String(150), nullable = True, default = '')
     last_name = db.Column(db.String(150), nullable = True, default = '')
@@ -20,6 +26,7 @@ class User(db.Model):
     g_auth_verify = db.Column(db.Boolean, default = False)
     token = db.Column(db.String, default = '', unique = True)
     date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    car = db.relationship('Car', backref = "owner", lazy = True)
 
     def __init__(self, email, first_name = '', last_name = '', id = '', password = '', token = '', g_auth_verify = False):
         self.id = self.set_id()
@@ -42,3 +49,48 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.email} has been added to the Database.'
+        
+
+class Car(db.Model):
+    id = db.Column(db.String, primary_key = True)
+    name = db.Column(db.String(150))
+    description = db.Column(db.String(200), nullable = True)
+    price = db.Column(db.Numeric(precision = 10, scale = 2))
+    camera_quality = db.Column(db.String(150), nullable = True)
+    flight_time = db.Column(db.String(100))
+    max_speed = db.Column(db.String(100))
+    dimensions = db.Column(db.String(100))
+    weight = db.Column(db.String(100))
+    cost_of_production = db.Column(db.Numeric(precision = 10, scale = 2))
+    series = db.Column(db.String(150))
+    user_token = db.Column(db.String, db.ForeignKey('user.token'), nullable = False)
+
+    def __init__(self, name, description, price, camera_quality, flight_time, max_speed, dimensions, weight, cost_of_production, series, user_token, id = ''):
+        self.id = self.set_id()
+        self.name = name
+        self.description = description
+        self.price = price
+        self.camera_quality = camera_quality
+        self.flight_time = flight_time
+        self.max_speed = max_speed
+        self.dimensions = dimensions
+        self.weight = weight
+        self.cost_of_production = cost_of_production
+        self.series = series
+        self.user_token = user_token
+
+    def set_id(self):
+        return secrets.token_urlsafe()
+
+    def __repr__(self):
+        return f'The following Car has been created: {self.name}'
+
+
+class CarSchema(ma.Schema):
+    class Meta:
+        fields = ['id','name','description','price','camera_quality','flight_time','max_speed','dimensions','weight','cost_of_production','series']
+
+
+car_schema = CarSchema()
+cars_schema = CarSchema(many = True)
+
